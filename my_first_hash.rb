81d294a7d369921e77a325c2d6e6ce1bc6df30af
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-04 10:58:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,10 @@
 def my_hash
-  parker = {"name" => "parkey"
+  parker = {"name" => "parkey" "job" => "sleeping"
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {"whale bone coresets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
